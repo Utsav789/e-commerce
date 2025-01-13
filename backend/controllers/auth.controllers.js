9383973ba6b2b9022f9d0cf0f63bff514b596acc
utsav789 <luintelutsav5@gmail.com>
2025-01-13 12:22:36 +0545

    second commit addding signup controller

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -1,18 +1,82 @@
-export const signup = async (req, res) => {
-  res.send("signup");
-  // try {
+import User from "../models/user.model.js";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+import { redis } from "../db/redis.js";
 
-  //   res.status(200).json({ message: "signup" });
-  // } catch (error) {
-  //   console.log("error");
-  // }
+dotenv.config();
+
+const generateTokens = (userId) => {
+  const accessToken = jwt.sign({ userId }, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: "15m",
+  });
+  const refreshToken = jwt.sign({ userId }, process.env.REFRESH_TOKEN_SECRET, {
+    expiresIn: "7d",
+  });
+
+  return { accessToken, refreshToken };
+};
+
+const storeRefreshToken = async (userId, refreshToken) => {
+  await redis.set(
+    `refresh_token:${userId}`,
+    refreshToken,
+    "EX",
+    7 * 24 * 60 * 60
+  ); // 7days
+};
+
+const setCookies = (res, accessToken, refreshToken) => {
+  res.cookie("accessToken", accessToken, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+    maxAge: 15 * 60 * 1000,
+  });
+
+  res.cookie("refreshToken", refreshToken, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+  });
+};
+
+export const signup = async (req, res) => {
+  const { email, password, name } = req.body;
+  try {
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+      return res.status(400).json({ message: "User Already Exists" });
+    }
+    const user = await User.create({ name, email, password });
+
+    //? authenticate
+
+    const { accessToken, refreshToken } = generateTokens(user._id);
+    await storeRefreshToken(user._id, refreshToken);
+
+    //? storing in cookies
+    setCookies(res, accessToken, refreshToken);
+
+    res.status(200).json({
+      user: {
+        _id: user.id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+      message: "User created",
+    });
+  } catch (error) {
+    console.log("Error in signup controller", error.message);
+    res.status(500).json({ success: false, message: "Internal server Error" });
+  }
 };
 export const login = async (req, res) => {
   res.send("login");
-
-}
+};
 
 export const logout = async (req, res) => {
   res.send("logout");
-
-}
+};
